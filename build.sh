955a0c0611681d0a828abcd2303845ea9238e824
mcrolins <rolinsmac21@gmail.com>
2026-02-04 11:00:45 +0300

    populated build.sh, ensure gunicorn is installed during build

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+set -o errexit
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m pip install gunicorn
+
+python manage.py collectstatic --noinput
+python manage.py migrate
 
